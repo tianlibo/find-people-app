58e44c2e9ca4d83a8d5d7d31ad0f2bdb9a1c95ac
tianlibo <tianlibosmile@163.com>
2016-10-14 11:48:58 +0800

    添加后台页面

--- db/migrate/20161012104355_create_users.rb
+++ db/migrate/20161012104355_create_users.rb
@@ -2,6 +2,9 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
+      t.string :email
+      t.string :phone
+      t.string :password_digest
 
       t.timestamps
     end
