2fab70a7e4d942546567958bf8e61e6dcfdc8191
tianlibo <tianlibosmile@163.com>
2016-10-18 16:12:56 +0800

    修改时间区bug  和 添加地图

--- app/controllers/admin/maps_controller.rb
+++ app/controllers/admin/maps_controller.rb
@@ -1,8 +1,6 @@
 class Admin::MapsController < Admin::ApplicationController
 
   def index
-
+    @positions = Position.all_people_newest_positions.collect{|position|[position.longitude.to_f,position.latitude.to_f]}
   end
-
-
 end
