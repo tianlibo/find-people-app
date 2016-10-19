301b9556c237e20bec99c9ccacbbb56d1130feb0
tianlibo <tianlibosmile@163.com>
2016-10-19 17:37:30 +0800

    提交

--- app/controllers/admin/maps_controller.rb
+++ app/controllers/admin/maps_controller.rb
@@ -1,6 +1,7 @@
 class Admin::MapsController < Admin::ApplicationController
 
   def index
-    @positions = Position.all_people_newest_positions.collect{|position|[position.longitude.to_f,position.latitude.to_f]}
+    # @positions = Position.all_people_newest_positions.collect{|position|[position.longitude.to_f,position.latitude.to_f]}
+    @positions = Position.all_newest_latlong
   end
 end
--- app/models/position.rb
+++ app/models/position.rb
@@ -5,4 +5,8 @@ class Position < ActiveRecord::Base
   def self.all_people_newest_positions
     Position.find_by_sql("SELECT DISTINCT ON (user_id) * FROM positions ORDER BY user_id,id DESC")
   end
+
+  def self.all_newest_latlong
+    Position.find_by_sql("SELECT DISTINCT ON (user_id) latitude, longitude FROM positions ORDER BY user_id,id DESC").map{|p| [p.longitude.to_f,p.latitude.to_f]}
+  end
 end
