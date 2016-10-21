5a74479030502f21c5df4f2c8bad9106225d80d0
tianlibo <tianlibosmile@163.com>
2016-10-21 20:20:05 +0800

    添加吃人数据库操作

--- app/models/position.rb
+++ app/models/position.rb
@@ -7,10 +7,14 @@ class Position < ActiveRecord::Base
   end
 
   def self.all_newest_latlong
-    Position.find_by_sql("SELECT DISTINCT ON (user_id) latitude, longitude FROM positions ORDER BY user_id,id DESC").map{|p| [p.longitude.to_f,p.latitude.to_f]}
+    Position.find_by_sql("SELECT DISTINCT ON (user_id) latitude, longitude FROM positions ORDER BY user_id,id DESC").map{|p|[p.longitude.to_f,p.latitude.to_f,p.created_at]}
+  end
+
+  def self.all_position_info
+    Position.find_by_sql("SELECT DISTINCT ON (user_id) latitude, longitude, positions.created_at, name, crumbs_count FROM positions, users WHERE positions.user_id = users.id ORDER BY user_id,positions.id DESC;").map{|p|[[p.longitude.to_f,p.latitude.to_f],[p.name.to_s,p.crumbs_count,p.created_at.to_s]]}
   end
 
   def self.find_to_eat(lng,lat)
-    Position.find_by_sql("SELECT * FROM positions WHERE earth_distance(ll_to_earth("+lat.to_s+","+lng.to_s+"), ll_to_earth(positions.latitude, positions.longitude)) < 1")
+    Position.find_by_sql("SELECT * FROM positions WHERE earth_distance(ll_to_earth("+lat.to_s+","+lng.to_s+"), ll_to_earth(positions.latitude, positions.longitude)) < 5 AND positions.created_at  > (now() - interval '1 minutes' * 5)")
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   
   has_many :positions
+
+  def self.find_to_eat(lng,lat,user)
+    @user = User.find_by_sql("SELECT users.* FROM users,positions WHERE positions.user_id = users.id AND earth_distance(ll_to_earth("+lat.to_s+","+lng.to_s+"), ll_to_earth(positions.latitude, positions.longitude)) < 5 AND positions.created_at > (now() - interval '1 minutes' * 5)").uniq - [user]
+  end
 end
