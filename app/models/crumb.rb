657fdf8780d1b5c67ecb57e988da6a2aa069a6d4
tianlibo <tianlibosmile@163.com>
2016-10-21 14:19:20 +0800

    提交

--- app/models/crumb.rb
+++ app/models/crumb.rb
@@ -4,7 +4,7 @@ class Crumb < ActiveRecord::Base
   #随机创建100个records
   def self.random_create_records(lng,lat)
     for i in 0..99
-      Crumb.create(longitude:(lng+Random.rand(0.000599)),latitude:(lat+Random.rand(0.000599)),accuracy:Random.rand(100))
+      Crumb.create(longitude:(lng+Random.rand(0.00599)),latitude:(lat+Random.rand(0.00599)),accuracy:Random.rand(100))
     end 
   end
 
