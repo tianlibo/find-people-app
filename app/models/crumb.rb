68c966effafbcd725a1a037f407d07d348dd616e
tianlibo <tianlibosmile@163.com>
2016-10-26 14:58:51 +0800

    提交

--- app/models/crumb.rb
+++ app/models/crumb.rb
@@ -14,10 +14,13 @@ class Crumb < ActiveRecord::Base
 
   def self.find_to_eat(lng,lat,user)
     #@crumbs = Crumb.find_by_sql("SELECT * FROM crumbs WHERE ate = false AND earth_distance(ll_to_earth("+lat.to_s+","+lng.to_s+"), ll_to_earth(crumbs.latitude, crumbs.longitude)) < 5")
-    @crumbs = Crumb.find_by_sql("WITH CTE_crumbs AS (SELECT * FROM crumbs WHERE latitude BETWEEN "+(lat - 0.0005).to_s + " AND " + (lat + 0.0005).to_s + " AND longitude BETWEEN " + (lng - 0.0005).to_s + " AND " + (lng + 0.0005).to_s + " AND ate = false) SELECT * FROM CTE_crumbs WHERE earth_distance(ll_to_earth("+lat.to_s+","+lng.to_s+"),ll_to_earth(latitude,longitude)) < 5")
+    @crumbs = Crumb.find_by_sql("WITH CTE_crumbs AS (SELECT * FROM crumbs WHERE latitude BETWEEN "+(lat - 0.0005).to_s + " AND " + (lat + 0.0005).to_s + " AND longitude BETWEEN " + (lng - 0.0005).to_s + " AND " + (lng + 0.0005).to_s + " AND ate = false) SELECT * FROM CTE_crumbs WHERE earth_distance(ll_to_earth("+lat.to_s+","+lng.to_s+"),ll_to_earth(latitude,longitude)) < 10")
     user.update crumbs_count: user.crumbs_count + @crumbs.size
     @crumbs.each do |c|
       c.update ate: true
     end
   end
+
+
+
 end
