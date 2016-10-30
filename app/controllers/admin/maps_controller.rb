4f68f1ab5c138fe279f5add4e4e5426efdf7a194
tianlibo <tianlibosmile@163.com>
2016-10-30 21:53:11 +0800

    添加个人位置吃看

--- app/controllers/admin/maps_controller.rb
+++ app/controllers/admin/maps_controller.rb
@@ -3,7 +3,14 @@ class Admin::MapsController < Admin::ApplicationController
   def index
     #@positions = Position.all_people_newest_positions.collect{|position|[position.longitude.to_f,position.latitude.to_f]}
     #@positions = Position.all_newest_latlong
-    @positions = Position.all_position_info
-    @crumbs = Crumb.all_lnglat
+    if params[:id].blank? 
+      @positions = Position.all_position_info
+      @crumbs = Crumb.all_lnglat
+    else  
+      p = Position.where(user_id: params[:id]).includes(:user).last
+      @positions = [[[p.longitude.to_f,p.latitude.to_f],[p.user.name.to_s,p.user.crumbs_count,p.created_at.to_s]]]
+      @crumbs = []
+    end
+    @center = @positions[0][0]
   end
 end
