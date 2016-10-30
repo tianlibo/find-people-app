d955c05f98b1927de935bc362b83cdf7ccf4f29e
tianlibo <tianlibosmile@163.com>
2016-10-30 23:51:02 +0800

    提交

--- app/api/v1/crumbs.rb
+++ app/api/v1/crumbs.rb
@@ -22,6 +22,23 @@ module V1
           }
         }
       end
+
+      params do 
+        requires :crumb, type: Hash do
+          requires :latitude
+          requires :longitude
+        end
+      end
+
+      desc 'create a new crumbs'
+      post :create do 
+        @crumb = Crumb.new latitude:params[:crumb][:latitude], longitude:params[:crumb][:longitude]
+        if @crumb.save
+          {code: 0, info: ""}
+        else
+          {code: 1, info: @user.errors.messages}
+        end
+      end
     end
   end
 end
